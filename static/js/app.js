8e678245e5dabc2ace95f0bbf1ba869222caacca
Taylor Finnell <tmfinnell@gmail.com>
2013-06-08 19:01:33 -0400

    "Remixed" a lot of JS

--- static/js/app.js
+++ static/js/app.js
@@ -1,214 +1,194 @@
+/* App.js - backbone */
+
 var App = (function(){
+
     var App = function(){
-        //Remixed.Logger.log("Made")
+        
     }
 
     App.prototype.init = function(first_argument) {
         
-        buttonAdd = $('#btn-add');
+        this.searchBox = $('#playlist-link')
+        this.loginForm = $('#login-form')
+        this.playlistForm = $('#playlist-form')
 
-        buttonAdd.bind('click', function(e){
-           
-           e.preventDefault()
-            
-            songs = []
-            $("input[type=checkbox]:checked:not(.first-checkbox)").each(function()
-            {
-                console.log(this)
-                songs.push({'id' : $(this).attr('data-id'), 'type' : '2'})
-            })
-
-            $.ajax({
-                'url' : 'crunch',
-                type : 'POST',
-                data: JSON.stringify({'playlist_title' :  $('#playlist-form').attr('playlist-id'), 'tracks' : songs}),
-                success : function(d){
-                    Remixed.UI.message({
-                        'response' : d,
-                        'rehide' : true
-                    })
-                }
-            })
-        })
-
-        checkboxCheckAll = $('#checkbox-check-all')
-        checkboxCheckAll.bind('click', function(){
-            $('input[type=checkbox]').filter(function(){ return !this.disabled }).attr('checked', true)
-        })
-
-        playlistValidityLabel = $('.control-group-pl')
-
-        loginForm = $('#login-form')
-        loginForm.bind('submit', function(e){
+        // Bind login button
+        this.loginForm.bind('submit', function(e){
             e.preventDefault();
-            $.ajax({
-                'url' : 'login',
-                'data' : loginForm.serialize(),
-                'type' : 'post',
-                success : function(d){
-                    
-                    Remixed.UI.message({
-                        'rehide' : false,
-                        'response' : d
-                    })
-                }
-            })
+
+            Remixed.App.login();
 
             return false;
         });
 
-        searchBox = $('#playlist-link')
-        searchBox.bind('input', function(e){
-
-            playlistValidityLabel.show();
-
-            $.ajax({
-                'url' : 'cpl',
-                'data' : JSON.stringify({
-                    'link' : searchBox.val()
-                }),
-                type : 'post',
-                success : function(d){
-                    if(d.success){
-                        playlistValidityLabel.addClass('success')
-                        searchBox.attr('data-valid', "true")
-                    }
-                    else{
-                        playlistValidityLabel.addClass('error')
-                        searchBox.attr('data-valid', "false")
-                    }
-                }   
-            })
-        });
-        
-        playlistForm = $('#playlist-form')
-        playlistForm.bind('submit', function(e){
+        //Bind change event for
+        this.searchBox.bind('input', function(e){
             e.preventDefault();
 
-            if(searchBox.attr('data-valid') != "true"){
-                Remixed.UI.message({
-                    'message' : "please input a valid playlist url",
-                    'success' : false
-                })
-                
-                return;
-            }
-            else
-                playlistValidityLabel.fadeOut();
+            Remixed.App.verify_playlist();
 
-            $.ajax({
-                'url' : 'playlist',
-                'data' : JSON.stringify({
-                    'link' : $('#playlist-link').val()
-                }),
-                type : 'POST',
-                success : function(d) {
-
-                    if(!d.success){
-                        Remixed.UI.message({
-                            'response' : d,
-                            'rehide' : true,
-                            'timeout' : 3000
-                        })
-
-                        return;
-                    }
-
-                    console.log(JSON.stringify(d.tracks));
-
-                    playlist_title = d.playlist_title
-
-                    playlistForm.attr('playlist-id', playlist_title )
-
-                    Remixed.UI.message({
-                        'message' : "Preparing to process &mdash; " + playlist_title,
-                        'success' : true,
-                        'css' : 'muted'
-                    })
-
-                    $('#table-results').show();
-
-                    for (var i = 0; i < d.tracks.length; i++) {
-
-                        if( !d.tracks[i].valid )
-                            continue;
-
-                        var dummy_art = 'https://ssl.gstatic.com/music/fe/f7ce26e1c6738383c444c02f238a678b/default_album_art_big_card.png'
-                        var html = 
-                                   [
-                                    '<tr class="warning" data-id="-1" id="row-' + i + '">',
-                                        '<td class="table-results-cell"><input disabled class="checkbox-result" type="checkbox" id="checkbox-' + i + '"></input></td>',
-                                        '<td class="table-results-cell thumb"><img src="' + dummy_art + '" id="img-' + i + '"/></td>',
-                                        '<td>',
-                                            '<div><strong>' + d.tracks[i].title + '</strong></div>',
-                                            '<div>',
-                                                '<p class="small muted"><small>'+ d.tracks[i].artist +'</small></p>',
-                                            '</div>',
-                                        '</td>',
-                                    '</tr>'
-                                   ].join('\n');
-
-                        $('#table-results tr:last').after(html)
-                    }
-
-                    Remixed.UI.page()
-
-                    cur_num = 1
-                    $.each(d.tracks, function(){
-                        track = this;
-                        total_tracks = d.tracks.length
-
-                        $.ajax({
-                            'url' : 'find',
-                            'type' : 'post',
-                            'data' : JSON.stringify(track),
-                            error : function(){
-                                cur_num++;
-                            },
-                            success : function(d){
-                                 
-                                var index = d.match.index
-
-                                var row = $('#table-results > tbody > tr').eq(index + 1);
-
-                                row.toggleClass("warning", false)
-
-                                if (d.match != null && d.match.google != null && d.match.spotify != null)
-                                {
-                                    row.addClass("success")
-                                    $('#img-' + index).attr('src', d.match.google[2])
-                                    row.attr('data-id', d.match.google[0])
-                                    $('#checkbox-' + index).removeAttr('disabled').attr('data-id', d.match.google[0])
-                                }
-                                else
-                                {
-                                    row.addClass("error")
-                                }
-
-                                row.attr('data', JSON.stringify(d.match))
-
-
-                                var msg = 'proccessing '+ playlist_title + ' &mdash; track ' + cur_num + ' of ' +
-                                        total_tracks + ' (' + Math.floor((cur_num/total_tracks * 100)) + '%)'
-
-                                Remixed.UI.message({
-                                    'message' : msg,
-                                    'rehide' : false,
-                                    'css' : "muted"
-                                })
-
-                                cur_num  ++;
-
-                                if( cur_num == total_tracks ){
-                                    $('#btn-add').fadeIn();
-                                    $('.page-message').hide();
-                                }
-                            }
-                        })
-                    })
-                }
-            })
+            return false;
         });
+
+        // Bind form submission
+        this.playlistForm = $('#playlist-form')
+        this.playlistForm.bind('submit', function(e){
+
+            e.preventDefault();
+
+            Remixed.App.send_playlist();
+
+            return false;
+        });
+
     };
+
+    App.prototype.verify_playlist = function(){
+
+        var t = this;
+
+        $.ajax({
+            'url' : 'cpl',
+            'data' : JSON.stringify({
+                'link' : this.searchBox.val()
+            }),
+            type : 'post',
+            success : function(d){
+                if(d.success){
+                    t.searchBox.attr('data-valid', "true")
+                }
+                else{
+                    t.searchBox.attr('data-valid', "false")
+                }
+
+                // TODO: Update some visual indicator of success or failure
+            }   
+        })
+    }
+
+    App.prototype.login = function() {
+    
+        $.ajax({
+            'url' : 'login',
+            'data' : this.loginForm.serialize(),
+            'type' : 'post',
+            success : function(d){
+            
+                Remixed.UI.message({
+                    'rehide' : false,
+                    'response' : d
+                })
+            }
+        })
+
+    };
+
+    App.prototype.import = function(tracks, playlist){
+
+        $.ajax({
+            'url' : 'crunch',
+            type : 'POST',
+            data: JSON.stringify({'playlist_title' : playlist, 'tracks' : tracks}),
+            success : function(d){
+
+                Remixed.UI.message({
+                    'response' : d,
+                    'rehide' : true
+                })
+
+            }
+        })
+    };
+
+    App.prototype.get_spotify_pl_complete = function(tracks, playlist){
+            cur_track_num = 0;
+
+            hits = [], misses = [];
+
+            var t = this;
+            $.each(tracks, function(){
+                track = this;
+                total_tracks = tracks.length
+
+                $.ajax({
+                    'url' : 'find',
+                    'type' : 'post',
+                    'data' : JSON.stringify(track),
+                    error : function(){
+                        cur_track_num++;
+                    },
+                    success : function(d){
+                         
+                        var index = d.match.index
+
+                        if (d.match != null && d.match.google != null && d.match.spotify != null)
+                        {
+                            hits.push( {"id" : d.match.google[0], "type" : "2"})
+                        }
+                        else
+                        {
+                            // Error matching
+                        }
+
+
+                        // TODO: Update status card here
+
+                        cur_track_num  ++;
+
+                        if(cur_track_num == total_tracks)
+                        {
+                             t.import(hits, playlist);
+                        }
+                    }
+                })
+        })
+    }
+
+    App.prototype.send_playlist = function() {
+        
+        // Ensure we have a valid sp URI, this is checked better on server side as well
+        if(this.searchBox.attr('data-valid') != "true"){
+            Remixed.UI.message({
+                'message' : "please input a valid playlist url",
+                'success' : false
+            })
+    
+            return;
+        }
+        
+        var t = this;
+
+        $.ajax({
+            'url' : 'playlist',
+            'data' : JSON.stringify({
+                'link' : this.searchBox.val()
+            }),
+            type : 'POST',
+            success : function(d) {
+
+                if(!d.success){
+                    Remixed.UI.message({
+                        'response' : d,
+                        'rehide' : true,
+                        'timeout' : 3000
+                    })
+
+                    return;
+                }
+
+                Remixed.Logger.log(JSON.stringify(d.tracks));
+
+                Remixed.UI.message({
+                    'message' : "Preparing to process &mdash; " + d.playlist_title,
+                    'success' : true,
+                    'css' : 'muted'
+                })
+
+                t.get_spotify_pl_complete(d.tracks, d.playlist_title);
+            }
+        })
+        
+    };
+
     return App;
 })();   
--- static/js/ui.js
+++ static/js/ui.js
@@ -1,10 +1,12 @@
+/* UI.js - This does UI stuff */
+
 var UI = (function(){
 
     var UI = function(){
     }
 
     UI.prototype.init = function(){
-
+        this.pageMessage = $('.page-message')
     };
 
     UI.prototype.message = function(options){
@@ -18,18 +20,17 @@ var UI = (function(){
             redirect_wait : 2000
         }
 
-        pageMessage = $('.page-message')
         $.extend( settings, options );       
 
-        pageMessage.removeClass("success").removeClass("error").removeClass('muted')
+        this.pageMessage.removeClass("success").removeClass("error").removeClass('muted')
 
         if(settings.response != null)
-            pageMessage.fadeIn()
+            this.pageMessage.fadeIn()
             .addClass( settings.response.success == true ? "success" : "error" )
             .addClass(settings.css)
             .html( settings.response.message )  
         else
-            pageMessage.fadeIn()
+            this.pageMessage.fadeIn()
             .addClass(settings.success == true ? "success" : "error")
             .addClass(settings.css)
             .html(settings.message)
@@ -48,49 +49,9 @@ var UI = (function(){
 
         else if( settings.rehide ) {
             setTimeout(function(){
-                pageMessage.fadeOut();
+                this.pageMessage.fadeOut();
             }, settings.timeout );
         }
-    };
-
-    UI.prototype.page = function(){
-        var currentPage = 0;
-        var numPerPage = 10;
-        var table = $('#table-results')
-
-        table.bind('repaginate', function() {
-            table.find('tbody tr:not(.first-row)').hide().slice(currentPage * numPerPage, (currentPage + 1) * numPerPage).show();
-
-            $('.page-link').removeClass('active');
-            $('#page-parent-' + currentPage).addClass('active')
-
-        });
-
-        table.trigger('repaginate');
-
-        var numRows = table.find('tbody tr:not(.first-row)').length;
-        var numPages = Math.ceil(numRows / numPerPage);
-
-        var pages = $('#pages')
-
-        for (var page = 0; page < numPages; page++) {
-             var pageLink = '<li class="page-link" id="page-parent-' + page +'"> <a href="#" id="page-' + page + '">' + ( page + 1 ) + '</a></li>'
-
-            pages.append(pageLink);
-
-            $('#page-' + page).bind('click', {
-                next_page: page
-            }, function(e) {
-                e.preventDefault();
-                currentPage = e.data['next_page'];
-                table.trigger('repaginate');
-            });
-
-            if(currentPage == 0)
-                $('#page-parent-0').addClass('active')
-
-        }
-
     };
 
     return UI;
